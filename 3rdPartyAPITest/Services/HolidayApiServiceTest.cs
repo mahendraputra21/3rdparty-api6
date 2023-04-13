a57b01e594e0d0319b0839a8c80a144d929b330f
Dewa Mahendra <31196162+mahendraputra21@users.noreply.github.com>
2023-04-13 16:17:43 +0800

    Add files via upload

---
+++ 3rdPartyAPITest/Services/HolidayApiServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using _3rdPartyAPI.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _3rdPartyAPITest.Services
+{
+    public class HolidayApiServiceTest
+    {
+        private readonly IHolidayApiService holidayApiService;
+
+        public HolidayApiServiceTest()
+        {
+            var services = new ServiceCollection();
+            services.AddHttpClient();
+            services.AddSingleton<IHolidayApiService, HolidayApiService>();
+            var serviceProvider = services.BuildServiceProvider();
+            var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+            
+            holidayApiService = new HolidayApiService(httpClientFactory);
+        }
+
+        [Fact]
+        public async Task GetHolidays_ReturnsSuccessStatusCode()
+        {
+            // Arrange
+            var countryCode = "au";
+            var year = 2021;
+
+            // Act
+            var response = await holidayApiService.GetHttpResponse(countryCode, year);
+
+            // Assert
+            Assert.NotNull(response);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+
+}
